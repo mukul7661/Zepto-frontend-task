fc637b26873c2c8ed7435519978bda0ec8896d8b
Mukul yadav <mukulydv14@gmail.com>
2024-01-16 02:21:45 +0530

    feature: highlight item before removal from list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import ChipInput from "../components/ChipInput";
 
 export default function Home() {
   const [availableItems, setAvailableItems] = useState<string[]>([
-    "Item1",
-    "Item2",
-    "Item3",
-    "Item4",
-    "Item5",
-    "Item6",
-    "Item7",
-    "Item8",
-    "Item9",
+    "Laxmi devi",
+    "Omparkash Kumar",
+    "Promila yadav",
+    "Pradeep kumar",
+    "Sharmila yadav",
+    "Ranbir singh",
+    "Shubham yadav",
+    "Poonam yadav",
+    "Jai",
+    "Khushi yadav",
+    "Mukul yadav",
   ]);
   return (
     <div className="App">
--- src/components/ChipInput.tsx
+++ src/components/ChipInput.tsx
@@ -5,15 +5,14 @@ interface ChipInputProps {
   availableItems: string[];
   setAvailableItems: React.Dispatch<React.SetStateAction<string[]>>;
 }
-interface KeyboardEvent {
-  key: string;
-}
+
 const ChipInput: React.FC<ChipInputProps> = ({
   availableItems,
   setAvailableItems,
 }) => {
   const [inputValue, setInputValue] = useState("");
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
   const [focusedIndex, setFocusedIndex] = useState<number | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -24,13 +23,20 @@ const ChipInput: React.FC<ChipInputProps> = ({
         inputValue === "" &&
         selectedItems.length > 0
       ) {
-        const updatedSelectedItems = [...selectedItems];
-        const removedItem = updatedSelectedItems.splice(
-          selectedItems.length - 1,
-          1
-        )[0]; // Remove the last selected item
-        setSelectedItems(updatedSelectedItems);
-        setAvailableItems([...availableItems, removedItem]);
+        if (highlightedIndex === null) {
+          setHighlightedIndex(selectedItems.length - 1);
+        } else {
+          const updatedSelectedItems = [...selectedItems];
+          const removedItem = updatedSelectedItems.splice(
+            highlightedIndex,
+            1
+          )[0];
+          setSelectedItems(updatedSelectedItems);
+          setHighlightedIndex(null);
+          if (removedItem !== undefined) {
+            setAvailableItems((prevItems) => [...prevItems, removedItem]);
+          }
+        }
       }
     };
 
@@ -39,10 +45,10 @@ const ChipInput: React.FC<ChipInputProps> = ({
     return () => {
       document.removeEventListener("keydown", handleBackspace);
     };
-  }, [inputValue, selectedItems]);
+  }, [inputValue, selectedItems, highlightedIndex, setAvailableItems]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
     setFocusedIndex(null);
   };
 
@@ -52,8 +58,7 @@ const ChipInput: React.FC<ChipInputProps> = ({
     setInputValue("");
     setFocusedIndex(null);
 
-    const updatedAvailableItems = [...availableItems];
-    updatedAvailableItems.splice(index, 1);
+    const updatedAvailableItems = availableItems.filter((_, i) => i !== index);
     setAvailableItems(updatedAvailableItems);
   };
 
@@ -62,24 +67,44 @@ const ChipInput: React.FC<ChipInputProps> = ({
     const removedItem = updatedSelectedItems.splice(index, 1)[0];
     setSelectedItems(updatedSelectedItems);
 
-    setAvailableItems([...availableItems, removedItem]);
+    setAvailableItems((prevItems) => [...prevItems, removedItem]);
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "ArrowDown") {
-      setFocusedIndex((prevIndex) =>
-        prevIndex === null || prevIndex === availableItems.length - 1
-          ? 0
-          : prevIndex + 1
-      );
-    } else if (event.key === "ArrowUp") {
-      setFocusedIndex((prevIndex) =>
-        prevIndex === null || prevIndex === 0
-          ? availableItems.length - 1
-          : prevIndex - 1
-      );
-    } else if (event.key === "Enter" && focusedIndex !== null) {
+  const handleArrowDown = () => {
+    setFocusedIndex((prevIndex) =>
+      prevIndex === null || prevIndex === availableItems.length - 1
+        ? 0
+        : prevIndex + 1
+    );
+  };
+
+  const handleArrowUp = () => {
+    setFocusedIndex((prevIndex) =>
+      prevIndex === null || prevIndex === 0
+        ? availableItems.length - 1
+        : prevIndex - 1
+    );
+  };
+
+  const handleEnterKey = () => {
+    if (focusedIndex !== null) {
       handleItemClick(availableItems[focusedIndex], focusedIndex);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (e.key) {
+      case "ArrowDown":
+        handleArrowDown();
+        break;
+      case "ArrowUp":
+        handleArrowUp();
+        break;
+      case "Enter":
+        handleEnterKey();
+        break;
+      default:
+        break;
     }
   };
 
@@ -87,7 +112,12 @@ const ChipInput: React.FC<ChipInputProps> = ({
     <div className="chip-input">
       <div className="chips">
         {selectedItems.map((item, index) => (
-          <div key={index} className="chip">
+          <div
+            key={index}
+            className={`chip ${
+              highlightedIndex === index ? "highlighted" : ""
+            }`}
+          >
             {item}
             <button
               onClick={() => handleChipRemove(index)}
@@ -107,15 +137,7 @@ const ChipInput: React.FC<ChipInputProps> = ({
             placeholder="Type to filter"
             className="input-field"
           />
-          <div
-            className="item-list"
-            style={{
-              width: "200px",
-              position: "absolute",
-              maxHeight: "200px",
-              overflowY: "auto",
-            }}
-          >
+          <div className="item-list">
             {availableItems
               .filter((item) =>
                 item.toLowerCase().includes(inputValue.toLowerCase())
